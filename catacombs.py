b528ce3b744bd25bd3b4d888159a43ba6e898568
spaceybread <Spaceybread@gmail.com>
2022-07-27 11:01:13 +0530

    Update catacombs.py
    first working prototype
    currently, only basic enemies exist, and both sides have access to all moves (this will not be the case in the final prototype)
    high score style game, try to beat as many enemies as possible on limited health
    
    need to add more moves and bosses

--- catacombs.py
+++ catacombs.py
@@ -19,65 +19,198 @@
 #if player wins, they move onto the next fight with the exact same health and stats, with a chance to use a health pot to heal themselves
 #all stats will be increased by +1 after each boss (undecided)
 
-attkBoost = 1
+import random
 
 def cleave():
     return 20
 
 def eschew():
-    return 25
+    return 25 + random.randrange(3)
 
 def ruleta():
-    return 10
+    return 15 + random.randrange(10)
 
 def rage():
     return 0
 
-def move():
-    move = int(input('What will you do? '))
+def move(isEnemy):
+
+    if isEnemy == True:
+        move = random.choice([0, 1, 2, 3])
+        user = 'Enemy'
+    else:
+        move = int(input('What will you do? '))
+        user = 'You'
 
     if move == 0:
-        print('cleave')
+        print('{} used cleave!'.format(user))
         effect = cleave()
-        modifier = 1
+        modifier = 0
         type = 0
 
     if move == 1:
-        print('eschew')
+        print('{} used eschew!'.format(user))
         effect = eschew()
-        modifier = 0.9
+        modifier = -0.4
         type = 1
 
     if move == 2:
-        print('ruleta')
+        print('{} used ruleta!'.format(user))
         effect = ruleta()
-        modifier = 1
+        modifier = 0
         type = 0
 
     if move == 3:
-        print('rage')
+        print('{} used rage'.format(user))
         effect = rage()
-        modifier = 1.75
+        modifier = 1.5
         type = 2
 
 
     return [effect, modifier, type]
 
+#while True:
+#    outcome = move(False)
+
+#    if outcome[2] == 0:
+#        calcDamage = outcome[0] * attkBoost
+#        print('You did {} damage'.format(calcDamage))
+#        attkBoost = attkBoost * outcome[1]
+#        print('Attack modifier is {}'.format(attkBoost))
+
+#    if outcome[2] == 1:
+#        print('You healed {} hit points'.format(outcome[0]))
+#        attkBoost = attkBoost * outcome[1]
+#        print('Attack modifier is {}'.format(attkBoost))
+
+#    if outcome[2] == 2:
+#        print('You boosted attack')
+#        attkBoost = attkBoost * outcome[1]
+#        print('Attack modifier is {}'.format(attkBoost))
+
+
+playerHealth = 500
+enemiesDefeated = 0
+
 while True:
-    outcome = move()
 
-    if outcome[2] == 0:
-        calcDamage = outcome[0] * attkBoost
-        print('You did {} damage'.format(calcDamage))
-        attkBoost = attkBoost * outcome[1]
-        print('Attack modifier is {}'.format(attkBoost))
+    print('Enemies defeated: {}'.format(enemiesDefeated))
 
-    if outcome[2] == 1:
-        print('You healed {} hit points'.format(outcome[0]))
-        attkBoost = attkBoost * outcome[1]
-        print('Attack modifier is {}'.format(attkBoost))
+    attkBoost = 1
+    eAttkBoost = 1
 
-    if outcome[2] == 2:
-        print('You boosted attack')
-        attkBoost = attkBoost * outcome[1]
-        print('Attack modifier is {}'.format(attkBoost))
+    enemyHealth = 100 + (20 * enemiesDefeated)
+
+    playerSpeed = random.randrange(5)
+    enemySpeed = random.randrange(5) - 0.5
+
+    print('Your speed stat: {}'.format(playerSpeed))
+    print('Enemy speed stat: {}'.format(enemySpeed))
+    input()
+
+    print('Entering combat...')
+
+    while True:
+        print('Your health: {}'.format(playerHealth))
+        print('Enemy health: {}'.format(enemyHealth))
+
+        print('')
+
+        playerMove = move(False)
+
+        enemyMove = move(True)
+
+        if playerSpeed > enemySpeed:
+
+            if playerMove[2] == 0:
+                calcDamage = playerMove[0] * attkBoost
+                enemyHealth = enemyHealth - calcDamage
+                print('You did {} damage'.format(calcDamage))
+                attkBoost = attkBoost + playerMove[1]
+                print('Attack modifier is {}'.format(attkBoost))
+
+            if playerMove[2] == 1:
+                playerHealth = playerHealth + playerMove[0]
+                print('You healed {} hit points'.format(playerMove[0]))
+                attkBoost = attkBoost + playerMove[1]
+                print('Attack modifier is {}'.format(attkBoost))
+
+            if playerMove[2] == 2:
+                print('You boosted attack')
+                attkBoost = attkBoost + playerMove[1]
+                print('Attack modifier is {}'.format(attkBoost))
+
+            if enemyHealth < 1:
+                print('You win!')
+                print('Onto the next enemy!')
+
+            else:
+                if enemyMove[2] == 0:
+                    calcDamage = enemyMove[0] * eAttkBoost
+                    playerHealth = playerHealth - calcDamage
+                    print('Enemy did {} damage'.format(calcDamage))
+                    eAttkBoost = eAttkBoost + enemyMove[1]
+                    print('Attack modifier is {}'.format(eAttkBoost))
+
+                if enemyMove[2] == 1:
+                    enemyHealth = enemyHealth + enemyMove[0]
+                    print('Enemy healed {} hit points'.format(enemyMove[0]))
+                    eAttkBoost = eAttkBoost + enemyMove[1]
+                    print('Attack modifier is {}'.format(eAttkBoost))
+
+                if enemyMove[2] == 2:
+                    print('Enemy boosted attack')
+                    eAttkBoost = eAttkBoost + enemyMove[1]
+                    print('Attack modifier is {}'.format(eAttkBoost))
+
+                if playerHealth < 1:
+                    print('Enemy Wins T-T')
+                    exit()
+        else:
+            if enemyMove[2] == 0:
+                calcDamage = enemyMove[0] * eAttkBoost
+                playerHealth = playerHealth - calcDamage
+                print('Enemy did {} damage'.format(calcDamage))
+                eAttkBoost = eAttkBoost + enemyMove[1]
+                print('Attack modifier is {}'.format(eAttkBoost))
+
+            if enemyMove[2] == 1:
+                enemyHealth = enemyHealth + enemyMove[0]
+                print('Enemy healed {} hit points'.format(enemyMove[0]))
+                eAttkBoost = eAttkBoost + enemyMove[1]
+                print('Attack modifier is {}'.format(eAttkBoost))
+
+            if enemyMove[2] == 2:
+                print('Enemy boosted attack')
+                eAttkBoost = eAttkBoost + enemyMove[1]
+                print('Attack modifier is {}'.format(eAttkBoost))
+
+            if playerHealth < 1:
+                print('Enemy Wins T-T')
+                exit()
+
+            else:
+                if playerMove[2] == 0:
+                    calcDamage = playerMove[0] * attkBoost
+                    enemyHealth = enemyHealth - calcDamage
+                    print('You did {} damage'.format(calcDamage))
+                    attkBoost = attkBoost + playerMove[1]
+                    print('Attack modifier is {}'.format(attkBoost))
+
+                if playerMove[2] == 1:
+                    playerHealth = playerHealth + playerMove[0]
+                    print('You healed {} hit points'.format(playerMove[0]))
+                    attkBoost = attkBoost + playerMove[1]
+                    print('Attack modifier is {}'.format(attkBoost))
+
+                if playerMove[2] == 2:
+                    print('You boosted attack')
+                    attkBoost = attkBoost + playerMove[1]
+                    print('Attack modifier is {}'.format(attkBoost))
+
+                if enemyHealth < 1:
+                    print('You win!')
+                    print('Onto the next enemy!')
+                    #exit()
+
+    enemiesDefeated = enemiesDefeated + 1
